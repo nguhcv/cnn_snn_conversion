fd8d4ed230bff95b4ff38509787e24e250114205
Huynh <46445099+nguhcv@users.noreply.github.com>
2022-05-24 19:22:28 +0900

    Update nets.py

--- source_code/nets.py
+++ source_code/nets.py
@@ -1,1 +1,23 @@
+from torch import nn
 
+class Diehl_2015(nn.Module):
+    def __init__(self, bias=False):
+        super(Diehl_2015, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=12, kernel_size=5, stride=1, padding=0, bias=bias)
+        self.avrpool1_1 = nn.AvgPool2d(2)
+        self.conv2 = nn.Conv2d(in_channels=12, out_channels=64, kernel_size=5, stride=1, padding=0, bias=bias)
+        self.avrpool2_1 = nn.AvgPool2d(2)
+        self.fc1 = nn.Linear(in_features=1024, out_features=10, bias=bias)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = nn.ReLU(x)
+        x = self.drop(x)
+        x = self.avrpool1_1(x)
+        x = self.conv2(x)
+        x = nn.ReLU(x)
+        x = self.drop(x)
+        x = self.avrpool2_1(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        return x
